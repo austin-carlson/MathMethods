a78ad1a212f5f21e90ba6db0f0a553a8b434bbaa
Austin Carlson <austincarl24@gmail.com>
2022-01-03 20:45:39 -0600

    I added some mathy

---
+++ MathMethods/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MathMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var sum = Add(10, 20);
+
+            Console.WriteLine(sum);
+
+            var difference = Subtract(10, 20);
+
+            Console.WriteLine(difference);
+
+            var product = Multiply(10, 20);
+
+            Console.WriteLine(product);
+
+            var quotient = Divide(10, 20);
+
+            Console.WriteLine(quotient);
+
+            
+            
+       
+        }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;   
+        }
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static int Divide(int num1, int num2)
+        { 
+            return num1 % num2; 
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+
+
+}
